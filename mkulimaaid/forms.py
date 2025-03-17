9b40910d367fb6c3af0e464fbf15f2fb7231eb55
fabius lihanda <fabiuslihandaachevi@gmail.com>
2025-03-17 10:02:30 +0300

    add region dropdown list on submit farm info

--- mkulimaaid/forms.py
+++ mkulimaaid/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError, Regexp, Optional
 from mkulimaaid.models import User
 import re
-from wtforms import StringField, PasswordField, SubmitField, DecimalField, FileField, BooleanField, TextAreaField
+from wtforms import StringField, PasswordField, SubmitField, DecimalField, FileField, BooleanField, TextAreaField, SelectField
 from flask_wtf.file import FileAllowed
 from wtforms import IntegerField
 from config import Config
@@ -231,7 +231,59 @@ class EmptyForm(FlaskForm):
 
 
 class FarmersForm(FlaskForm):
-    location = StringField('Farm Location', validators=[DataRequired(), Length(max=200)])
+    # List of all regions in Kenya for the dropdown
+    REGIONS = [
+        ('', 'Select Region'),  # Default placeholder
+        ('Baringo', 'Baringo'),
+        ('Bomet', 'Bomet'),
+        ('Bungoma', 'Bungoma'),
+        ('Busia', 'Busia'),
+        ('Elgeyo Marakwet', 'Elgeyo Marakwet'),
+        ('Embu', 'Embu'),
+        ('Garissa', 'Garissa'),
+        ('Homa Bay', 'Homa Bay'),
+        ('Isiolo', 'Isiolo'),
+        ('Kajiado', 'Kajiado'),
+        ('Kakamega', 'Kakamega'),
+        ('Kericho', 'Kericho'),
+        ('Kiambu', 'Kiambu'),
+        ('Kilifi', 'Kilifi'),
+        ('Kirinyaga', 'Kirinyaga'),
+        ('Kisii', 'Kisii'),
+        ('Kisumu', 'Kisumu'),
+        ('Kitui', 'Kitui'),
+        ('Kwale', 'Kwale'),
+        ('Laikipia', 'Laikipia'),
+        ('Lamu', 'Lamu'),
+        ('Machakos', 'Machakos'),
+        ('Makueni', 'Makueni'),
+        ('Mandera', 'Mandera'),
+        ('Marsabit', 'Marsabit'),
+        ('Meru', 'Meru'),
+        ('Migori', 'Migori'),
+        ('Mombasa', 'Mombasa'),
+        ('Murang\'a', 'Murang\'a'),
+        ('Nairobi', 'Nairobi'),
+        ('Nakuru', 'Nakuru'),
+        ('Nandi', 'Nandi'),
+        ('Narok', 'Narok'),
+        ('Nyamira', 'Nyamira'),
+        ('Nyandarua', 'Nyandarua'),
+        ('Nyeri', 'Nyeri'),
+        ('Samburu', 'Samburu'),
+        ('Siaya', 'Siaya'),
+        ('Taita Taveta', 'Taita Taveta'),
+        ('Tana River', 'Tana River'),
+        ('Tharaka Nithi', 'Tharaka Nithi'),
+        ('Trans Nzoia', 'Trans Nzoia'),
+        ('Turkana', 'Turkana'),
+        ('Uasin Gishu', 'Uasin Gishu'),
+        ('Vihiga', 'Vihiga'),
+        ('Wajir', 'Wajir'),
+        ('West Pokot', 'West Pokot')
+    ]
+
+    location = SelectField('Region', choices=REGIONS, validators=[DataRequired()], render_kw={"class": "form-select"})
     farm_size = DecimalField('Farm Size (in acres)', validators=[DataRequired()], places=2)
     crop_types = StringField('Types of Crops Grown', validators=[DataRequired(), Length(max=200)])
     description = TextAreaField('Farm Description', validators=[Optional(), Length(max=500)])
